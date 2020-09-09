b5100bfd8752a984026bf0e18fe491eff99388bb
rani.lt <rani.loewidt-tzadok@viaccess-orca.com>
2020-09-09 09:41:31 +0300

    simple FE

--- src/app.js
+++ src/app.js
@@ -1,35 +1,7 @@
 const express = require('express')
-const kafka = require('kafka-node')
-const _ = require('lodash');
 const path = require('path');
 const router = express.Router();
 
-const topic = 'topic1';
-
-const Producer = kafka.Producer,
-    KeyedMessage = kafka.KeyedMessage,
-    client = new kafka.KafkaClient(),
-    producer = new Producer(client),
-    km = new KeyedMessage('key', 'message')
-
-
-const Consumer = kafka.Consumer,
-    consumer = new Consumer(
-        client,
-        [
-            { topic: topic, partition: 0 }
-        ],
-        {
-            autoCommit: true
-        }
-    );
-
-const publish = async (producer, payloads) => {
-    producer.send(payloads, function (err, data) {
-        console.log(data);
-        if (err) console.error("Error on send", err);
-    });
-}
 
 
 const app = express()
@@ -37,30 +9,21 @@ app.use('/', router);
 const port = 3000
 
 router.get('/pub', (req, res) => {
-    const statuses = ['started', 'processing', 'finished'];
-    statuses.forEach((status) => {
-        const payloads = [
-            {
-                topic: topic,
-                messages: JSON.stringify({
-                    value: status,
-                    key: req.query.val
-                })
-            }
-        ];
-        publish(producer, payloads)
+    res.status(200).send(req.headers)
+})
+
+app.get('/:name', function (req, res, next) {
+    var options = {
+      root: path.join(__dirname, 'public')
+    }
+    var fileName = req.params.name || "index.html"
+    res.sendFile(fileName, options, function (err) {
+      if (err) {
+        next(err)
+      } else {
+        console.log('Sent:', fileName)
+      }
     })
-    res.status(200).send('ok')
-})
-router.get('/', (req, res) => res.sendFile((path.join(__dirname + '/index.html'))))
-
-consumer.on('message', (message) => {
-    console.log("Consume", message);
-})
-
-producer.on('error', function (err) { console.error("Error on producer", err) })
-producer.on('ready', function () {
-    console.log('producer ready');
-});
+  })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
